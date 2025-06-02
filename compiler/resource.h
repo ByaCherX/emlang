2ba45d6dad16d28666544a4da4745f11527cc852
EmreKayal <kralkedi_1428@hotmail.com>
2025-06-02 18:36:47 +0300

    Add resource files and update CMake configuration for emlang compiler

---
+++ compiler/resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by emlang_compiler.rc
+//
+
+#define IDS_COMPILER_NAME               101
+#define IDS_VERSION_STRING              102
+#define IDS_ARCHITECTURE                103
+
+// Compiler Information
+#define EMLANG_COMPILER_NAME            "EMLang Compiler"
+#define EMLANG_COMPILER_DESCRIPTION     "EMLang Programming Language Compiler Library"
+#define EMLANG_COMPILER_FILENAME        "emlang_compiler.dll"
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
