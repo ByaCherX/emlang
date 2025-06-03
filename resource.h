41762430aa407557ffc9a42c0c87047e7386d9e1
EmreKayal <kralkedi_1428@hotmail.com>
2025-06-03 13:46:47 +0300

    Temporarily comment out string table definitions in resource file for future implementation

---
+++ resource.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by emlang_compiler.rc
+//
+
+#define IDS_APP_NAME                    201
+#define IDS_VERSION_STRING              202
+#define IDS_ARCHITECTURE                203
+#define IDS_USAGE                       204
+#define IDS_COPYRIGHT                   205
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        206
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
